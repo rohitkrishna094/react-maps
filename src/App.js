1a2e255f7049b333ce66099a98a38025ffcb277b
rohitkrishna094 <rohitkrishna094@gmail.com>
2018-10-07 13:53:02 -0400

    modified app.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,6 @@ import './App.css';
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 import MyMap from './components/MyMap';
 class App extends Component {
-  state = {
-    lat: 51.505,
-    lng: -0.09,
-    zoom: 13
-  };
-
   render() {
     return <MyMap />;
   }
