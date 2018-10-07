2aebaafe602be0525db600a628fb3208dbae7e5b
rohitkrishna094 <rohitkrishna094@gmail.com>
2018-10-07 14:46:05 -0400

    changed css

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 import MyMap from './components/MyMap';
+
 class App extends Component {
   render() {
     return <MyMap />;
