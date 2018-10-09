ffa0dee17bfb986c34ac8cb4c3c4a9a4a94a7300
Shamanth Kumar <32136251+ShamanthK@users.noreply.github.com>
2018-10-09 15:55:29 -0400

    Added Header Logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import Logoimg from "./HeaderLia.JPG"
 
 const Header = () => {
   return (
     <div>
-      <p>Header works</p>
+    <header className="headersize">
+
+    <img  src={Logoimg}  alt="fireSpot" className="imgsize"/>
+
+    </header>
     </div>
   );
 };
 
 export default Header;
+
