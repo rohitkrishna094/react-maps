c36c78627a6cf60b45a829b194720285800b51d9
Shamanth Kumar <32136251+ShamanthK@users.noreply.github.com>
2018-10-09 15:52:41 -0400

    Dashboard Changes

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,13 +1,51 @@
 import React, { Component } from 'react';
+import {Form, Input,Row, Col} from 'antd';
 
 class Dashboard extends Component {
   render() {
     return (
-      <div>
-        <p>Dashboard Works</p>
-      </div>
+    <form>
+
+    <label>Group Members
+    <input type="text" name="GrpMembers"/>
+    </label>
+
+    <label>Individual Members
+    <input type="text" name="IndvMembers"/>
+    </label>
+
+    <label>State
+    <select>
+    <option value="all">All</option>
+  <option value="saab">Saab</option>
+  <option value="mercedes">Mercedes</option>
+  <option value="audi">Audi</option>
+</select>
+
+    </label>
+
+    <label>Medicare Members
+    <input type="text" name="MedicareMembers"/>
+    </label>
+
+    <label>Medicaid Members
+    <input type="text" name="MedicaidMembers"/>
+    </label>
+
+    <label>Combination
+    <select>
+    <option value="all">All</option>
+  <option value="saab">Saab</option>
+  <option value="mercedes">Mercedes</option>
+  <option value="audi">Audi</option>
+  </select>
+
+    </label>
+
+    </form>
     );
   }
 }
 
 export default Dashboard;
+
